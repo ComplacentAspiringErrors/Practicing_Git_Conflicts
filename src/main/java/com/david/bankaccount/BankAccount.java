7bc61455243b93294fc0bb1d7100a021ff345f84
David Phillip <daphiscc-g-025@student.wethinkcode.co.za>
2026-06-27 16:38:11 +0200

    constructor wrong trying to activate a git merge conflict

--- src/main/java/com/david/bankaccount/BankAccount.java
+++ src/main/java/com/david/bankaccount/BankAccount.java
@@ -1,4 +1,12 @@
 package com.david.bankaccount;
 
 public class BankAccount {
+    double amount;
+    String name;
+
+
+    public BankAccount(double amount, String name){
+        this.amount = amount;
+        this.name = name;
+    }
 }
